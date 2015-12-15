2a6b27aedc7beb785817168e93c80de5032ff602
Alessandra Davila <aledavila>
2015-12-14 22:01:53 -0600

    w03d01

---
+++ w03/d01/starter/js/main.js
@@ -0,0 +1,88 @@
+$(function() {
+
+    var box = $("input");
+    var log = $('textarea');
+    var add, minus, multiply, divide;
+    var num;
+    var r = 0;
+    var result;
+
+    $("#clear").click(function() {
+        box.val('');
+    });
+
+    $("#zero, #one, #two, #three, #four, #five, #six, #seven, #eight, #nine").click(function() {
+        box.val(box.val() + $(this).val());
+
+    });
+
+    $("#plus").click(function() {
+        add = true;
+        minus = false;
+        multiply = false;
+        divide = false;
+        if (box.val()) {
+            num = parseInt(box.val());
+            box.val('');
+        };
+    });
+
+    $("#subtract").click(function() {
+        add = false;
+        minus = true;
+        multiply = false;
+        divide = false;
+        if (box.val()) {
+            num = parseInt(box.val());
+            box.val('');
+        };
+    });
+
+    $("#multiply").click(function() {
+        add = false;
+        minus = false;
+        multiply = true;
+        divide = false;
+        if (box.val()) {
+            num = parseInt(box.val());
+            box.val('');
+        };
+    });
+
+    $("#divide").click(function() {
+        add = false;
+        minus = false;
+        multiply = false;
+        divide = true;
+        if (box.val()) {
+            num = parseInt(box.val());
+            box.val('');
+        };
+    });
+
+    $("#equals").click(function() {
+        var num2 = parseInt(box.val());
+        if (num && add) {
+            box.val(num + num2);
+        } else if (num && minus) {
+            box.val(num - num2);
+        } else if (num && multiply) {
+            box.val(num * num2);
+        } else if (num && divide) {
+            box.val(num / num2);
+        } else {
+            //$(body).insertBefore("img", );
+        }
+
+    r++;
+    result = box.val();
+    log.prepend( r + ") " + result + "\n");
+    localStorage.setItem("R" + r + ": ", result);
+
+    $("#clear-log").click(function() {
+      window.localStorage.clear();
+      log.val('');
+    });
+
+    });
+});
