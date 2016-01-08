9958e041d9290a2973a3b8460c337f91d42e6195
Alessandra Davila <aledavila>
2016-01-07 20:02:28 -0600

    w04d03 hw

--- w04/d03/starter/app.rb
+++ w04/d03/starter/app.rb
@@ -2,16 +2,40 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 # Coin toss
-get '/coin_toss' do
-	
+get '/toss' do
+	["heads","tails"].shuffle.first
 end
 
 # Dice roll
-get '/dice_roll' do
-	
+get '/roll' do
+  ["1", "2", "3", "4", "5", "6"].shuffle.first
 end
 
-# Magic 8 ball 
-get '/magic8ball/:question' do
-
+# Magic 8 ball
+get '/8ball/:question' do
+  @loser = params[:question]
+  @eight = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes", "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+  ].shuffle.first
+  erb :magic8ball
 end
+
+get '/rps/rock'
