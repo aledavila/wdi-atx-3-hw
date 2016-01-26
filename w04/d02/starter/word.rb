86a4a8ed8fc65f5966eb417f848dd57f0caec167
Alessandra Davila <davilaaguilar23@gmail.com>
2016-01-26 11:32:28 -0600

    w04do2 hw

--- w04/d02/starter/word.rb
+++ w04/d02/starter/word.rb
@@ -1,9 +1,50 @@
-class Word
+class
+  attr_reader :original_word
+
   def initialize(original_word)
     @original_word = original_word
   end
 
+  vowels = %w[a e i o u y]
+  consonants = alpha - vowels
+
   def original_word
     return @original_word.downcase
   end
+
+  if consonants.include?(@original_word[0]) && consonants.include?(@original_word[1]) && consonants.include?(@original_word[2])
+       first = @original_word.slice!(0)
+       second = @original_word.slice!(0)
+       third = @original_word.slice!(0)
+       @original_word + "-#{first}#{second}#{third}ay"
+     elsif consonants.include?(@original_word[0]) && consonants.include?(@original_word[1])
+       first = @original_word.slice!(0)
+       second = @original_word.slice!(0)
+       @original_word + "-#{first}#{second}ay"
+     elsif consonants.include?(@original_word[0])
+       character = @original_word.slice!(0)
+       @original_word + "-#{character}ay"
+     elsif vowels.include?(@original_word[0])
+       @original_word + "-yay"
+     if consonants.include?(original_word[0]) && consonants.include?(original_word[1]) && consonants.include?(original_word[2])
+       first = original_word.slice!(0)
+       second = original_word.slice!(0)
+       third = original_word.slice!(0)
+       original_word + "-#{first}#{second}#{third}ay"
+     elsif consonants.include?(original_word[0]) && consonants.include?(original_word[1])
+       first = original_word.slice!(0)
+       second = original_word.slice!(0)
+       original_word + "-#{first}#{second}ay"
+     elsif consonants.include?(original_word[0])
+       character = original_word.slice!(0)
+       original_word + "-#{character}ay"
+     elsif vowels.include?(original_word[0])
+       original_word + "-yay"
+      end
+  end
+
 end
+
+word = Word.new('loser')
+ +puts word.instance_variables
+  puts word.translate
