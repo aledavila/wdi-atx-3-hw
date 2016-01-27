792577bddd5aef4e2fbbbcad099358d60ebc27d4
Alessandra Davila <davilaaguilar23@gmail.com>
2016-01-26 20:53:23 -0600

    adds the homework that was the hardest thing ever for w07d02. i tried

--- w07/d02/starter/geom.js
+++ w07/d02/starter/geom.js
@@ -4,7 +4,19 @@ function Rectangle(length, width) {
 }
 
 Rectangle.prototype =  {
-	// Your code goes here
+	isSquare: function() {
+    if(this.length === this.width) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  area: function() {
+    return this.length * this.width;
+  },
+  perimeter: function() {
+    return 2 * (this.length + this.width);
+  }
 };
 
 function Triangle(sideA, sideB, sideC) {
@@ -13,7 +25,28 @@ function Triangle(sideA, sideB, sideC) {
   this.sideC = sideC;
 }
 Triangle.prototype = {
-	// Your code goes here
+	isEquilateral: function() {
+    if((this.sideA === this.sideB) && (this.sideA === this.sideC) && (this.sideB === this.sideC)) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  isIsosceles: function() {
+    if((this.sideA && this.sideB) || (this.sideA && this.sideC) || (this.sideB && this.sideC) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+  area: function() {
+    var s = (this.sideA + this.sideB + this.sideC) / 2;
+    var area = (s * (s - this.sideA) * (s - this.sideB) * (s - this.sideC));
+    return Math.sqrt(area);
+  },
+  isObtuse: function() {
+
+  }
 };
 
 
@@ -26,3 +59,13 @@ function LineSegment(x1, y1, x2, y2) {
 LineSegment.prototype = {
 	// Your code goes here
 };
+
+loser = new Rectangle(2, 2);
+console.log(loser.isSquare());
+console.log(loser.area());
+console.log(loser.perimeter());
+
+triangle = new Triangle(4, 13, 15);
+console.log(triangle.isEquilateral());
+console.log(triangle.isIsosceles());
+console.log(triangle.area());
