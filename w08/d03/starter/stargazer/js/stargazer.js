f04693ec38736f83e51b9d7acb96fb395416de62
Alessandra Davila <davilaaguilar23@gmail.com>
2016-02-03 19:52:49 -0600

    w08d03 hw

--- w08/d03/starter/stargazer/js/stargazer.js
+++ w08/d03/starter/stargazer/js/stargazer.js
@@ -1,5 +1,28 @@
-function search(name) {
-  var api = 'http://www.strudel.org.uk/lookUP/json/?name='+name;
-  // requires "jsonp" datatype.
-  
-}
+$(function(){
+
+  $('#search').on('submit', function(e){
+
+    var input = $('#search-name').val();
+
+    $.ajax({
+        url: 'http://www.strudel.org.uk/lookUP/json/',
+        method: 'GET',
+        dataType: "jsonp",
+        data: {
+          name: input
+        },
+
+    })
+    .done(function(data){
+      if (TypeError) {
+        $('#preview').html("<p>Could not find " + input + "</p>");
+      }
+      if (data["image"]["src"].includes('upload')) {
+        $('#preview').html("<a class='result' target='_blank' href='" + data["image"]["href"] + "'>No picture available</a>");
+      } else {
+        $('#preview').html("<img class='result' src='" + data["image"]["src"] + "'/>");
+      }
+    })
+    e.preventDefault();
+  });
+});
